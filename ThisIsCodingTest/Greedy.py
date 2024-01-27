fc97ef6a02ef11fa11a49e7afa47a4d154bc8cd7
SeongYongPark <pji50260@naver.com>
2024-01-27 14:16:11 +0900

    1_27

--- ThisIsCodingTest/Greedy.py
+++ ThisIsCodingTest/Greedy.py
@@ -13,26 +13,51 @@ print(change)
 # 예제 3-2 큰수의 법칙 (수정 필요)
 N,M,K = map(int, input().split())
 result = 0
-arr = []
 
-for _ in range(N):
-    number = map(int, input().split())
-    arr.append(number)
-
+arr = list(map(int, input().split()))
 arr.sort()
 
-arr_index = 0
-cnt = 0
+first = arr[N-1]
+second = arr[N-2]
 
-for _ in range(M):
-  
-  if(cnt <= K):
-    result = result + arr[arr_index]
-    cnt += 1
-  else:
-    arr_index += 1
-    cnt = 0
+while True:
+  for _ in range(K):
+    if M==0:
+      break
+    result += first
+    M -= 1
+  if M==0:
+    break
+  result += second
+
 
 print(result)
 
 # 예제 3-3 숫자 카드 게임
+N, M = map(int, input().split())
+
+
+cards = [[0] * M for _ in range(N)]
+minimum = [0] * N
+
+for i in range(N):
+    cards[i] = list(map(int, input().split()))
+    minimum[i] = min(cards[i])
+
+    
+print(max(minimum))
+
+# 예제 3-4 1이 될 때까지
+N, K = map(int, input().split())
+
+Count = 0
+
+while N != 1:
+  if N % K == 0:
+    N = N // K
+    Count += 1
+  else:
+    N -=1
+    Count +=1
+
+print(Count)
